cf321a5938be47025264657be0790032d7401ee4
Munkh-orgil <munkhorgil@live.com>
2019-06-26 23:26:23 +0800

    Expr reply/send email

--- src/gmail/client/containers/index.tsx
+++ src/gmail/client/containers/index.tsx
@@ -5,24 +5,21 @@ import { sendEmail } from '../util';
 
 class BaseContainer extends React.Component {
   render() {
-    const send = (params, reply) => {
-      return sendEmail(params, reply);
-    };
-
-    const props = {
+    const updatedProps = {
       user: { email: 'bfyhdgzj@gmail.com' , firstName: 'Harry' }, // current user
       type: 'reply',
-      send,
+      send: sendEmail,
       message: { // Reply
         to: 'munkhorgil@live.com',
         from: 'bfyhdgzj@gmail.com',
-        subject: 'demo2',
-        headerId: "<HK0PR03MB39709CE12589CF40B04D04DDA9E20@HK0PR03MB3970.apcprd03.prod.outlook.com>",
-        threadId: "16b93a62a967fa39"
+        subject: 'Re: soul',
+        references: "<CAMnbcUs=YgndTg8asB3cOKCUV-LtROqTtva4Gz4b0FdCRxUi+w@mail.gmail.com>,<HK0PR03MB3970F4637417D23C3D49544BA9E20@HK0PR03MB3970.apcprd03.prod.outlook.com>",
+        headerId: "<HK0PR03MB3970AADAF2523F3D38704454A9E20@HK0PR03MB3970.apcprd03.prod.outlook.com>",
+        threadId: "16b94470c4355b15"
       }
     };
 
-    return <Wrapper {...props} />;
+    return <Wrapper {...updatedProps} />;
   }
 }
 
