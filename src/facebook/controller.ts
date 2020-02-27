ca94e977cc520b10785dfaa86386891f1ae488b2
BatAmar Battulga <batamar.b@nmtec.co>
2020-02-27 15:43:53 +0800

    added getAdapter function in facebook controller

--- src/facebook/controller.ts
+++ src/facebook/controller.ts
@@ -235,25 +235,29 @@ const init = async app => {
     return res.json(result.reverse());
   });
 
-  const FACEBOOK_VERIFY_TOKEN = await getConfig('FACEBOOK_VERIFY_TOKEN');
-  const FACEBOOK_APP_SECRET = await getConfig('FACEBOOK_APP_SECRET');
-
-  if (!FACEBOOK_VERIFY_TOKEN || !FACEBOOK_APP_SECRET) {
-    return debugBase('Invalid facebook config');
-  }
-
   const accessTokensByPageId = {};
 
-  const adapter = new FacebookAdapter({
-    verify_token: FACEBOOK_VERIFY_TOKEN,
-    app_secret: FACEBOOK_APP_SECRET,
-    getAccessTokenForPage: async (pageId: string) => {
-      return accessTokensByPageId[pageId];
-    },
-  });
+  const getAdapter = async (): Promise<any> => {
+    const FACEBOOK_VERIFY_TOKEN = await getConfig('FACEBOOK_VERIFY_TOKEN');
+    const FACEBOOK_APP_SECRET = await getConfig('FACEBOOK_APP_SECRET');
+
+    if (!FACEBOOK_VERIFY_TOKEN || !FACEBOOK_APP_SECRET) {
+      return debugBase('Invalid facebook config');
+    }
+
+    return new FacebookAdapter({
+      verify_token: FACEBOOK_VERIFY_TOKEN,
+      app_secret: FACEBOOK_APP_SECRET,
+      getAccessTokenForPage: async (pageId: string) => {
+        return accessTokensByPageId[pageId];
+      },
+    });
+  };
 
   // Facebook endpoint verifier
-  app.get('/facebook/receive', (req, res) => {
+  app.get('/facebook/receive', async (req, res) => {
+    const FACEBOOK_VERIFY_TOKEN = await getConfig('FACEBOOK_VERIFY_TOKEN');
+
     // when the endpoint is registered as a webhook, it must echo back
     // the 'hub.challenge' value it receives in the query arguments
     if (req.query['hub.mode'] === 'subscribe') {
@@ -273,6 +277,8 @@ const init = async app => {
     }
 
     debugFacebook(`Received webhook data ${JSON.stringify(data)}`);
+
+    const adapter = await getAdapter();
 
     for (const entry of data.entry) {
       // receive chat
