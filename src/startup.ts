590484781257e22fd6f719c6290bc81abd0a816f
Munkh-Orgil <munkhorgil@live.com>
2020-03-09 13:38:03 +0800

    refactor: use then instead of await

--- src/startup.ts
+++ src/startup.ts
@@ -1,10 +1,17 @@
-import { debugIntegrations } from './debuggers';
+import { debugGmail, debugIntegrations } from './debuggers';
 import { trackGmail } from './gmail/watch';
 
 export const init = async () => {
   try {
-    await trackGmail();
+    trackGmail()
+      .then(() => {
+        debugGmail('Successfully called trackGmail');
+      })
+      .catch(e => {
+        throw e;
+      });
   } catch (e) {
     debugIntegrations(e.message);
+    throw e;
   }
 };
