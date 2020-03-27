babf289a39cf7453a9019e6dabb9f0f309d1a377
soyombo <soyombo.b@nmtec.co>
2020-03-27 17:39:37 +0800

    Update receiveMessage.ts

--- src/smooch/receiveMessage.ts
+++ src/smooch/receiveMessage.ts
@@ -6,52 +6,53 @@ import { Integrations } from '../models';
 const receiveMessage = async requestBody => {
   const { trigger } = requestBody;
 
-  if (trigger === 'message:appUser') {
-    const { appUser, messages, conversation, client } = requestBody;
+  if (trigger !== 'message:appUser') {
+    return;
+  }
+  const { appUser, messages, conversation, client } = requestBody;
 
-    const smoochIntegrationId = client.integrationId;
+  const smoochIntegrationId = client.integrationId;
 
-    const customer: ISmoochCustomerInput = {
+  const customer: ISmoochCustomerInput = {
+    smoochIntegrationId,
+    surname: appUser.surname,
+    givenName: appUser.givenName,
+    smoochUserId: appUser._id,
+  };
+
+  if (client.platform === 'twilio') {
+    customer.phone = client.displayName;
+  } else if (client.platform === 'telegram' && client.raw.profile_photos.total_count !== 0) {
+    const { file_id } = client.raw.profile_photos.photos[0][0];
+
+    const { telegramBotToken } = await Integrations.findOne({ smoochIntegrationId });
+
+    customer.avatarUrl = await getTelegramFile(telegramBotToken, file_id);
+  } else if (client.platform === 'line' && client.raw.pictureUrl) {
+    customer.avatarUrl = client.raw.pictureUrl;
+  } else if (client.platform === 'viber' && client.raw.avatar) {
+    customer.avatarUrl = client.raw.avatar;
+  }
+
+  for (const message of messages) {
+    const content = message.text;
+    const received = message.received;
+
+    const customerId = await saveCustomer(customer);
+
+    const conversationIds = await saveConversation(
       smoochIntegrationId,
-      surname: appUser.surname,
-      givenName: appUser.givenName,
-      smoochUserId: appUser._id,
-    };
+      conversation._id,
+      customerId,
+      content,
+      received,
+    );
 
-    if (client.platform === 'twilio') {
-      customer.phone = client.displayName;
-    } else if (client.platform === 'telegram' && client.raw.profile_photos.total_count !== 0) {
-      const { file_id } = client.raw.profile_photos.photos[0][0];
-
-      const { telegramBotToken } = await Integrations.findOne({ smoochIntegrationId });
-
-      customer.avatarUrl = await getTelegramFile(telegramBotToken, file_id);
-    } else if (client.platform === 'line' && client.raw.pictureUrl) {
-      customer.avatarUrl = client.raw.pictureUrl;
-    } else if (client.platform === 'viber' && client.raw.avatar) {
-      customer.avatarUrl = client.raw.avatar;
-    }
-
-    for (const message of messages) {
-      const content = message.text;
-      const received = message.received;
-
-      const customerId = await saveCustomer(customer);
-
-      const conversationIds = await saveConversation(
-        smoochIntegrationId,
-        conversation._id,
-        customerId,
-        content,
-        received,
-      );
-
-      if (message.type !== 'text') {
-        const attachment: IAttachment = { type: message.mediaType, url: message.mediaUrl };
-        await saveMessage(smoochIntegrationId, customerId, conversationIds, content, message._id, attachment);
-      } else {
-        await saveMessage(smoochIntegrationId, customerId, conversationIds, content, message._id);
-      }
+    if (message.type !== 'text') {
+      const attachment: IAttachment = { type: message.mediaType, url: message.mediaUrl };
+      await saveMessage(smoochIntegrationId, customerId, conversationIds, content, message._id, attachment);
+    } else {
+      await saveMessage(smoochIntegrationId, customerId, conversationIds, content, message._id);
     }
   }
 };
