646c0cd0b49d3205649c097c68bcc952494d58cc
soyombo <40427263+soyombo-baterdene@users.noreply.github.com>
2020-08-07 15:44:24 +0800

    Update models.ts

--- src/productBoard/models.ts
+++ src/productBoard/models.ts
@@ -1,6 +1,56 @@
 import { Document, Model, model, Schema } from 'mongoose';
+import {
+  Comments,
+  ConversationMessages as FacebookMessengerConversationMessages,
+  Conversations as FacebookMessengerConversations,
+  Customers as FacebookMessengerCustomers,
+  Posts,
+} from '../facebook/models';
 import { field } from '../models/utils';
-
+import {
+  NylasExchangeConversationMessages,
+  NylasExchangeConversations,
+  NylasExchangeCustomers,
+  NylasGmailConversationMessages,
+  NylasGmailConversations,
+  NylasGmailCustomers,
+  NylasImapConversationMessages,
+  NylasImapConversations,
+  NylasImapCustomers,
+  NylasOffice365ConversationMessages,
+  NylasOffice365Conversations,
+  NylasOffice365Customers,
+  NylasOutlookConversationMessages,
+  NylasOutlookConversations,
+  NylasOutlookCustomers,
+  NylasYahooConversationMessages,
+  NylasYahooConversations,
+  NylasYahooCustomers,
+} from '../nylas/models';
+import {
+  SmoochLineConversationMessages,
+  SmoochLineConversations,
+  SmoochLineCustomers,
+  SmoochTelegramConversationMessages,
+  SmoochTelegramConversations,
+  SmoochTelegramCustomers,
+  SmoochTwilioConversationMessages,
+  SmoochTwilioConversations,
+  SmoochTwilioCustomers,
+  SmoochViberConversationMessages,
+  SmoochViberConversations,
+  SmoochViberCustomers,
+} from '../smooch/models';
+import {
+  ConversationMessages as TwitterConversationMessages,
+  Conversations as TwitterConversations,
+  Customers as TwitterCustomers,
+} from '../twitter/models';
+import {
+  ConversationMessages as WhatsAppConversationMessages,
+  Conversations as WhatsAppConversations,
+  Customers as WhatsAppCustomers,
+} from '../whatsapp/models';
 export interface IConversation {
   erxesApiId?: string;
   timestamp: Date;
@@ -45,3 +95,75 @@ export const Conversations = model<IConversationDocument, IConversationModel>(
   'conversations_productboard',
   conversationSchema,
 );
+
+export const ALL_MODELS = {
+  gmail: {
+    customers: NylasGmailCustomers,
+    conversations: NylasGmailConversations,
+    conversationMessages: NylasGmailConversationMessages,
+  },
+  exchange: {
+    customers: NylasExchangeCustomers,
+    conversations: NylasExchangeConversations,
+    conversationMessages: NylasExchangeConversationMessages,
+  },
+  imap: {
+    customers: NylasImapCustomers,
+    conversations: NylasImapConversations,
+    conversationMessages: NylasImapConversationMessages,
+  },
+  outlook: {
+    customers: NylasOutlookCustomers,
+    conversations: NylasOutlookConversations,
+    conversationMessages: NylasOutlookConversationMessages,
+  },
+  yahoo: {
+    customers: NylasYahooCustomers,
+    conversations: NylasYahooConversations,
+    conversationMessages: NylasYahooConversationMessages,
+  },
+  office365: {
+    customers: NylasOffice365Customers,
+    conversations: NylasOffice365Conversations,
+    conversationMessages: NylasOffice365ConversationMessages,
+  },
+  telegram: {
+    customers: SmoochTelegramCustomers,
+    conversations: SmoochTelegramConversations,
+    conversationMessages: SmoochTelegramConversationMessages,
+  },
+  viber: {
+    customers: SmoochViberCustomers,
+    conversations: SmoochViberConversations,
+    conversationMessages: SmoochViberConversationMessages,
+  },
+  line: {
+    customers: SmoochLineCustomers,
+    conversations: SmoochLineConversations,
+    conversationMessages: SmoochLineConversationMessages,
+  },
+  twilio: {
+    customers: SmoochTwilioCustomers,
+    conversations: SmoochTwilioConversations,
+    conversationMessages: SmoochTwilioConversationMessages,
+  },
+  whatsapp: {
+    customers: WhatsAppCustomers,
+    conversations: WhatsAppConversations,
+    conversationMessages: WhatsAppConversationMessages,
+  },
+  twitter: {
+    customers: TwitterCustomers,
+    conversations: TwitterConversations,
+    conversationMessages: TwitterConversationMessages,
+  },
+  'facebook-messenger': {
+    customers: FacebookMessengerCustomers,
+    conversations: FacebookMessengerConversations,
+    conversationMessages: FacebookMessengerConversationMessages,
+  },
+  'facebook-post': {
+    posts: Posts,
+    comments: Comments,
+  },
+};
